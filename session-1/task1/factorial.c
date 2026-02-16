2d683b83d42291fbb849b9fe8da0714662c23e8a
Hafeezah <137874263+feweqweqef@users.noreply.github.com>
2026-02-16 12:47:46 +0000

    end of attempt 1

--- session-1/task1/factorial.c
+++ session-1/task1/factorial.c
@@ -8,9 +8,22 @@
  int main( void ) {
     int f[20];
 
+
     /*
     Code to compute the factorial of each array index
     Print your final answer
     */
+
+    f[0] = 1;
+    for (int i=1; i<20; ++i){
+
+      f[i] = i * f[i-1];
+
+    }
+
+    for (int i=0; i < 20; i++){
+      printf("%d! = %d\n", i, f[i]);
+    }
+
     return 0;
  }
